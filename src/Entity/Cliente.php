c80e3c0255cf6a578fecb68b3196c85741b12a76
krlossrs <krlossrs@gmail.com>
2020-09-01 00:23:43 -0400

    disenno

---
+++ src/Entity/Cliente.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClienteRepository")
+ */
+class Cliente
+{
+    /**
+     * @var \Ramsey\Uuid\UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="User", inversedBy="cliente", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(name="usuario_id", referencedColumnName="id", nullable=false)
+     */
+    private $usuario;
+
+    /** @ORM\Column(type="string", length=180) */
+    protected $url;
+
+
+    /** @ORM\Column(type="string", length=180) */
+    protected $imagen;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of usuario
+     */ 
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * Set the value of usuario
+     *
+     * @return  self
+     */ 
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    /**
+     * @param mixed $imagen
+     */
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+    }
+
+
+  
+    public function __toString()
+    {
+        return $this->usuario->getNombre()." ".$this->usuario->getApellidos();
+    }
+}
