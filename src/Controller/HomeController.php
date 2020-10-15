47c51829d8bde0dd32d0f981ea10064f31e0f6ed
krlossrs <krlossrs@gmail.com>
2020-10-14 23:34:56 -0400

    paginas nuevas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -52,4 +52,36 @@ class HomeController extends AbstractController
         return $this->render('home/mercado.html.twig', []);
     }
 
+    /**
+     * @Route("/importaciones", name="importaciones")
+     */
+    public function importacion()
+    {
+        return $this->render('home/importacion.html.twig', []);
+    }
+
+    /**
+     * @Route("/exportaciones", name="exportaciones")
+     */
+    public function exportacion()
+    {
+        return $this->render('home/exportacion.html.twig', []);
+    }
+
+    /**
+     * @Route("/logistica", name="logistica")
+     */
+    public function logistica()
+    {
+        return $this->render('home/logistica.html.twig', []);
+    }
+
+    /**
+     * @Route("/contacto", name="contacto")
+     */
+    public function contacto()
+    {
+        return $this->render('home/contacto.html.twig', []);
+    }
+
 }
